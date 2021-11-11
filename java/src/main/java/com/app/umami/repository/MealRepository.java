7398046281c82e7822e7178f5a3b5cc4eaf8c0af
Vivek Khamar <vivekhamar@gmail.com>
2021-11-11 19:59:01 +0530

    Code refactored

--- java/src/main/java/com/app/umami/repository/MealRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.umami.repository;
-
-import com.app.umami.entity.Meal;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MealRepository extends MongoRepository<Meal, String>, MealRepositoryCustom {
-}
